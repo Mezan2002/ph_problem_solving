a5e0a23f1a4e2b73beec7bce36940bdb39725af5
Mezan2002 <mezanurr009@gmail.com>
2023-01-29 01:00:43 +0600

    Problem Solved

---
+++ rotateLeft.js
@@ -0,0 +1,9 @@
+function rotateLeft(arr, n) {
+  for (let i = 0; i < n; i++) {
+    let first = arr.shift();
+    arr.push(first);
+  }
+  return arr;
+}
+
+console.log(rotateLeft([1, 2, 3, 4, 5], 1));
